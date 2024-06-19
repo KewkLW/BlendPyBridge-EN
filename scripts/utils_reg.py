008d2e80e20c4f0de50686893b8a74cba8afd307
Kewk <57611539+KewkLW@users.noreply.github.com>
2024-06-18 18:26:29 -0700

    comments

--- scripts/utils_reg.py
+++ scripts/utils_reg.py
@@ -1,54 +1,50 @@
 import gc
 import sys
 import inspect
-
 import bpy
 
-
-# Разрегистрация старой версии
-#### Анализ запускаемого файла для поиска классов для разрегистрации
+# Deregister old version
+#### Analyze the running file to find classes for deregistration
 def UNregister(package_name):
     print(f'\n\033[38;5;208m * def UNregister(package_name)  \033[0m')
 
     class_registered = []
 
     for cls_name in dir(bpy.types):
-        # Получаем ссылку на элемент в bpy.types
+        # Get a reference to the item in bpy.types
         cls = getattr(bpy.types, cls_name)
-        # Если ссылка является классом
+        # If the reference is a class
         if isinstance(cls, type):
-            # И начинается с имени модуля пользователя
+            # And it starts with the user's module name
             if cls.__module__.startswith(package_name):
                 # print(' + ', cls)
                 class_registered.append(cls.__name__)
 
                 try:
                     bpy.utils.unregister_class(cls)
-                    print(f'\t- {class_registered[-1]} разрегистрирован')
+                    print(f'\t- {class_registered[-1]} unregistered')
                 except Exception as e:
-                    print(f'\t* Ошибка при разрегистрации {class_registered[-1]}: {e}')
-
+                    print(f'\t* Error while unregistering {class_registered[-1]}: {e}')
 
     if class_registered:
         print('\n')
-        print('Господа идущие к плахе')
+        print('Gentlemen heading to the scaffold')
         print(class_registered)
         print('\n')
-
 
     addon_modules = [mod for mod in sys.modules if mod.startswith(package_name)]
 
     for module_name in addon_modules:
         try:
-            # Удаляем модуль из sys.modules
+            # Remove the module from sys.modules
             del sys.modules[module_name]
-            print(f'* Модуль {module_name} удалён из sys.modules')
+            print(f'* Module {module_name} removed from sys.modules')
         except KeyError:
-            # Модуль уже был удален из sys.modules
-            print(f'* Модуль {module_name} уже был удален из sys.modules')
+            # Module was already removed from sys.modules
+            print(f'* Module {module_name} was already removed from sys.modules')
         except Exception as e:
-            # Обработка других потенциальных исключений
-            print(f'* Ошибка при удалении модуля {module_name}: {e}')
+            # Handling other potential exceptions
+            print(f'* Error while removing module {module_name}: {e}')
 
-    print(f'\n\033[92m#### Магия иссякла ####\033[0m')
+    print(f'\n\033[92m#### The magic has ended ####\033[0m')
     print('\n')
